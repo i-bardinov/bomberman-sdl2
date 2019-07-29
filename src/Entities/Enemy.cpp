25e7943694494aaafe7d840f98099bd41e5a9454
Ilya Bardinov <ilya.bardinov@gmail.com>
2019-07-29 20:59:26 +0300

    missed includes

--- src/Entities/Enemy.cpp
+++ src/Entities/Enemy.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <functional>
 #include <random>
 
 #include "Const.h"
--- src/Scenes/LevelScene.cpp
+++ src/Scenes/LevelScene.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <chrono>
+#include <functional>
 #include <random>
 #include <string>
 
